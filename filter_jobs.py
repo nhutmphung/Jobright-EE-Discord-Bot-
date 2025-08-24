8e2ac77923d6eece7a9319e78ee3bb856b0e9c72
nhutmphung <nhutmphung@gmail.com>
2025-08-24 00:17:05 -0500

    fitler_jobs updated

--- filter_jobs.py
+++ filter_jobs.py
@@ -1,4 +1,34 @@
+from fetch_jobs import fetch_jobs
+import re
+
 
 #takes the mark down text as a string and parses the text
 def filter_jobs(string):
-    ...
+    jobs = []
+    lines = string.split("\n")
+
+    for line in lines:
+        if line.startswith("| ** "):  # example: each job starts with '|"
+            #uses regex to capture the position, company, location, url
+            match = re.match(r"- \*\*(.*?)\*\* at (.*?) \((.*?)\) - \[.*?\]\((.*?)\)", line)
+            if match:
+                title, company, location, url = match.groups()
+                jobs.append ({
+                    "title": title,
+                    "company": company, 
+                    "location": location,
+                    "url": url
+                }
+
+
+                )
+    print(jobs)
+    return jobs
+
+# def filter_jobs(jobs, remote_only=False):
+#     """
+#     Optionally filter jobs (e.g., only remote positions).
+#     """
+#     if remote_only:
+#         return [job for job in jobs if job["location"].lower() == "remote"]
+#     return jobs
